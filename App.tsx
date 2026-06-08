6b6a7875ce6ee2078f47273e79c45943ebdef7e4
Leticia Takahashi <leticiasatiko@gmail.com>
2026-06-08 09:14:40 -0300

    tamanho da tela

--- App.tsx
+++ App.tsx
@@ -1,45 +1,27 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import React, { Component } from "react";
+import { StyleSheet, View, Text } from "react-native";
+import params from "./src/params";
 
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
-import {
-  SafeAreaProvider,
-  useSafeAreaInsets,
-} from 'react-native-safe-area-context';
-
-function App() {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  return (
-    <SafeAreaProvider>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <AppContent />
-    </SafeAreaProvider>
-  );
-}
-
-function AppContent() {
-  const safeAreaInsets = useSafeAreaInsets();
-
-  return (
-    <View style={styles.container}>
-      <NewAppScreen
-        templateFileName="App.tsx"
-        safeAreaInsets={safeAreaInsets}
-      />
-    </View>
-  );
+export default class App extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.text}>Iniciando Mines!</Text>
+        <Text style={styles.text}>Tamanho da grade: 
+          {params.getColumnsAmount()}x{params.getRowsAmount()}</Text>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: "#F5FCFF",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    fontSize: params.fontSize,
   },
 });
-
-export default App;
